52b531898a6f4babe47168f03b0b9a5c5eb80a2b
Atakanz <atakanzer@gmail.com>
2022-09-30 03:41:35 +0300

    Userslice added to store.

--- ChatApp/src/Management/store.js
+++ ChatApp/src/Management/store.js
@@ -1,8 +1,10 @@
 import {configureStore} from '@reduxjs/toolkit';
 import themeSlice from './Features/themeSlice';
+import userSlice from './Features/userSlice';
 
 export const store = configureStore({
   reducer: {
     theme: themeSlice,
+    auth: userSlice,
   },
 });

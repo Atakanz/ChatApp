379631927ffd746288a0c1ecfee01e9941c6dd5e
Atakanz <atakanzer@gmail.com>
2022-09-30 03:48:51 +0300

    Pick image custom hook added.

---
+++ ChatApp/src/Hooks/pickImageFromGallery.js
@@ -0,0 +1,17 @@
+import {useState} from 'react';
+import * as ImagePicker from 'expo-image-picker';
+
+export default function usePickImage() {
+  const pickImage = async () => {
+    let result = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ImagePicker.MediaTypeOptions.All,
+      allowsEditing: true,
+      aspect: [16, 9],
+      quality: 1,
+    });
+    if (!result.cancelled) {
+      return result.uri;
+    }
+  };
+  return {pickImage};
+}

7488d1887fecc8a94371c67acdd1645d759a19d1
Atakanz <atakanzer@gmail.com>
2022-09-30 05:41:37 +0300

    User cards added.

---
+++ ChatApp/src/Components/UserCards/UserCards.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import {SafeAreaView, TouchableOpacity, Image, Text, View} from 'react-native';
+import {useSelector} from 'react-redux';
+import styles from './UserCards.style';
+
+const UserCards = props => {
+  const theme = useSelector(state => state.theme.theme);
+  return (
+    <SafeAreaView style={[styles.container, styles[`container${theme}`]]}>
+      <TouchableOpacity onPress={props.task}>
+        <View style={styles.enabledDirection}>
+          <View style={styles.viewImg}>
+            <Image source={{uri: props.link}} style={styles.img} />
+          </View>
+          <View style={[styles.textView, styles[`textView${theme}`]]}>
+            <Text style={[styles.textInfo, styles[`text${theme}`]]}>
+              {props.name} {props.surname}
+            </Text>
+          </View>
+        </View>
+      </TouchableOpacity>
+    </SafeAreaView>
+  );
+};
+
+export default UserCards;
---
+++ ChatApp/src/Components/UserCards/UserCards.style.js
@@ -0,0 +1,41 @@
+import {StyleSheet} from 'react-native';
+
+export default StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 12,
+  },
+  enabledDirection: {
+    flexDirection: 'row',
+  },
+  containerDark: {
+    backgroundColor: '#212121',
+  },
+  containerLight: {
+    backgroundColor: '#fff',
+  },
+  img: {
+    width: 75,
+    height: 75,
+    borderRadius: 70,
+  },
+  viewImg: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginRight: 20,
+    marginLeft: 10,
+  },
+  textInfo: {
+    fontSize: 17,
+    fontWeight: 'bold',
+  },
+  textDark: {
+    color: '#fff',
+  },
+  textLight: {
+    color: '#212121',
+  },
+  textView: {
+    justifyContent: 'center',
+  },
+});

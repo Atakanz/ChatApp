c0a7f41f1c838e354fe35c9968bdde3a4eec3525
Atakanz <atakanzer@gmail.com>
2022-10-01 05:16:34 +0300

    Chatroom docs constructed.

--- ChatApp/src/Pages/ContactsList/contactsList.js
+++ ChatApp/src/Pages/ContactsList/contactsList.js
@@ -2,20 +2,16 @@ import React, {useState, useEffect} from 'react';
 import {SafeAreaView, FlatList} from 'react-native';
 import styles from './contacts.style';
 import {db, auth} from '../../../config';
-import {
-  doc,
-  updateDoc,
-  collection,
-  query,
-  where,
-  getDocs,
-} from 'firebase/firestore';
+import {collection, query, where, getDocs, addDoc} from 'firebase/firestore';
 import {useIsFocused} from '@react-navigation/native';
 import UserCards from '../../Components/UserCards';
+import {useSelector, useDispatch} from 'react-redux';
+import {setAllChatRooms} from '../../Management/Features/chatSlice';
 
 const ContactsList = ({navigation}) => {
   const [allUsers, setAllUsers] = useState([]);
-
+  const allChats = useSelector(state => state.chat.allChatRooms);
+  const dispatch = useDispatch();
   const getData = async () => {
     const q = query(
       collection(db, 'users'),
@@ -32,7 +28,6 @@ const ContactsList = ({navigation}) => {
     getData();
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [isFocused]);
-  console.log('link', allUsers[0].photoUrl);
   return (
     <SafeAreaView style={styles.container}>
       <FlatList
@@ -42,7 +37,28 @@ const ContactsList = ({navigation}) => {
             name={item.name}
             surname={item.surname}
             link={item.photoUrl}
-            task={() => navigation.navigate('ChatPage')}
+            task={() => {
+              navigation.navigate('ChatPage', {
+                name: item.name,
+                link: item.photoUrl,
+                id: item.id,
+              });
+              const isExist = allChats.some(function (elem) {
+                return (
+                  (elem[0] === auth.currentUser.uid && elem[1] === item.id) ||
+                  (elem[0] === item.id && elem[1] === auth.currentUser.uid)
+                );
+              });
+              if (isExist === false) {
+                addDoc(collection(db, 'chatRooms'), {
+                  members: {member1: auth.currentUser.uid, member2: item.id},
+                  messages: [],
+                }).then(docRef => {
+                  console.log('Document written with ID: ', docRef.id);
+                  dispatch(setAllChatRooms([auth.currentUser.uid, item.id]));
+                });
+              }
+            }}
           />
         )}
       />

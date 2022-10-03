95895302206384467a22dad6c15b201b616cdcb3
Atakanz <atakanzer@gmail.com>
2022-10-03 07:11:58 +0300

    Chatpage footer added.

---
+++ ChatApp/src/Components/ChatPageFooter/ChatPageFooter.js
@@ -0,0 +1,48 @@
+import React from 'react';
+import {SafeAreaView, View, TextInput} from 'react-native';
+import styles from './ChatPageFooter.style';
+import {useSelector} from 'react-redux';
+import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
+
+const ChatPageFooter = props => {
+  const theme = useSelector(state => state.theme.theme);
+  const colorSelect = theme === 'Dark' ? '#fff' : '#212121';
+  const send = (
+    <Icon.Button
+      name="send"
+      size={30}
+      onPress={props.sendIconTask}
+      style={[styles.ıconButton, styles[`ıconButton${theme}`]]}
+      color={colorSelect}
+    />
+  );
+  const paperClip = (
+    <Icon.Button
+      name="paperclip"
+      size={25}
+      color={colorSelect}
+      onPress={props?.paperClipTask}
+      style={[styles.ıconButton, styles[`ıconButton${theme}`]]}
+    />
+  );
+  const smile = (
+    <Icon name="emoticon-excited-outline" size={25} color={colorSelect} />
+  );
+  return (
+    <SafeAreaView style={[styles.bottomBar, styles[`bottomBar${theme}`]]}>
+      <View>{smile}</View>
+      <TextInput
+        style={[styles.textInput, styles[`textInput${theme}`]]}
+        label="Mesaj"
+        value={props.messageText}
+        placeholder="Mesaj"
+        placeholderTextColor={theme === 'Dark' ? '#fff' : '#212121'}
+        onChangeText={props.textInputTask}
+      />
+      <View>{paperClip}</View>
+      <View>{send}</View>
+    </SafeAreaView>
+  );
+};
+
+export default ChatPageFooter;
---
+++ ChatApp/src/Components/ChatPageFooter/ChatPageFooter.style.js
@@ -0,0 +1,47 @@
+import {StyleSheet} from 'react-native';
+import {useSelector} from 'react-redux';
+
+export default StyleSheet.create({
+  bottomBar: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    paddingHorizontal: 8,
+    paddingLeft: 12,
+    alignItems: 'center',
+    borderTopWidth: 2,
+    borderTopColor: '#00B050',
+    height: 50,
+  },
+  bottomBarDark: {
+    backgroundColor: '#212121',
+  },
+  bottomBarLight: {
+    backgroundColor: '#fff',
+  },
+  textInput: {
+    width: '75%',
+    color: '#212121',
+  },
+  textInputDark: {
+    width: '75%',
+    color: '#fff',
+  },
+  ıconButton: {
+    flex: 0,
+    width: 40,
+    height: 30,
+    alignSelf: 'center',
+    alignItems: 'center',
+    justifyContent: 'center',
+    textAlign: 'center',
+    padding: 0,
+    paddingTop: 0,
+    color: 'white',
+  },
+  ıconButtonDark: {
+    backgroundColor: '#212121',
+  },
+  ıconButtonLight: {
+    backgroundColor: '#fff',
+  },
+});

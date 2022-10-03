018a5365f368d390b2863291aa3036be23ab14c5
Atakanz <atakanzer@gmail.com>
2022-10-03 07:13:48 +0300

    Currentchat reducer removed.

--- ChatApp/src/Management/Features/chatSlice.js
+++ ChatApp/src/Management/Features/chatSlice.js
@@ -1,7 +1,6 @@
 import {createSlice} from '@reduxjs/toolkit';
 
 const initialState = {
-  currentChat: null,
   allChatRooms: [],
 };
 
@@ -9,16 +8,12 @@ export const chatSlice = createSlice({
   name: 'chat',
   initialState,
   reducers: {
-    setCurrentChat: (state, action) => {
-      state.currentChat = action.payload;
-    },
     setAllChatRooms: (state, action) => {
-      state.allChatRooms = [...state.allChatRooms, action.payload];
-      //   state.allChatRooms = action.payload;
+      state.allChatRooms = action.payload;
     },
   },
 });
 
-export const {setCurrentChat, setAllChatRooms} = chatSlice.actions;
+export const {setAllChatRooms} = chatSlice.actions;
 
 export default chatSlice.reducer;

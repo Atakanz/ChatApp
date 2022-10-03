6278567c70f738e660943489c3a9bb68629594f1
Atakanz <atakanzer@gmail.com>
2022-10-03 07:17:39 +0300

    Formulized ids added to chatRooms.

--- ChatApp/src/Pages/ContactsList/contactsList.js
+++ ChatApp/src/Pages/ContactsList/contactsList.js
@@ -2,17 +2,28 @@ import React, {useState, useEffect} from 'react';
 import {SafeAreaView, FlatList} from 'react-native';
 import styles from './contacts.style';
 import {db, auth} from '../../../config';
-import {collection, query, where, getDocs, addDoc} from 'firebase/firestore';
+import {
+  doc,
+  collection,
+  query,
+  where,
+  getDocs,
+  setDoc,
+  updateDoc,
+} from 'firebase/firestore';
 import {useIsFocused} from '@react-navigation/native';
 import UserCards from '../../Components/UserCards';
 import {useSelector, useDispatch} from 'react-redux';
 import {setAllChatRooms} from '../../Management/Features/chatSlice';
+import {setAllUsers} from '../../Management/Features/userSlice';
 
 const ContactsList = ({navigation}) => {
-  const [allUsers, setAllUsers] = useState([]);
   const allChats = useSelector(state => state.chat.allChatRooms);
+  const allUsers = useSelector(state => state.auth.allUsers);
+
   const dispatch = useDispatch();
-  const getData = async () => {
+  const isFocused = useIsFocused();
+  const getUsers = async () => {
     const q = query(
       collection(db, 'users'),
       where('id', '!=', auth.currentUser.uid),
@@ -20,14 +31,45 @@ const ContactsList = ({navigation}) => {
     await getDocs(q).then(res => {
       const _users = res.docs.map(item => item.data());
       console.log('user', _users);
-      setAllUsers(_users);
+      dispatch(setAllUsers(_users));
+      console.log('allusers', allUsers);
     });
   };
-  const isFocused = useIsFocused();
+  const getChatRooms = async () => {
+    const q = query(collection(db, 'chatRooms'));
+    await getDocs(q).then(res => {
+      const chatRooms = res.docs.map(item => item.data());
+      console.log('cRooms', chatRooms);
+      dispatch(setAllChatRooms(chatRooms));
+    });
+  };
+
+  const setChatRoom = receiverId => {
+    const chatId =
+      auth.currentUser.uid > receiverId
+        ? auth.currentUser.uid + receiverId
+        : receiverId + auth.currentUser.uid;
+    const isExist = allChats.some(function (elem) {
+      return elem?.chat === chatId;
+    });
+    if (isExist === false) {
+      setDoc(doc(db, 'chatRooms', chatId), {
+        messages: [],
+        chat: chatId,
+      });
+    }
+  };
+
   useEffect(() => {
-    getData();
+    const unsubscribe = navigation.addListener('focus', () => {
+      getUsers();
+      getChatRooms();
+      console.log('dispatch', allChats);
+    });
+    return unsubscribe;
     // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [isFocused]);
+  }, [navigation]);
+
   return (
     <SafeAreaView style={styles.container}>
       <FlatList
@@ -43,21 +85,7 @@ const ContactsList = ({navigation}) => {
                 link: item.photoUrl,
                 id: item.id,
               });
-              const isExist = allChats.some(function (elem) {
-                return (
-                  (elem[0] === auth.currentUser.uid && elem[1] === item.id) ||
-                  (elem[0] === item.id && elem[1] === auth.currentUser.uid)
-                );
-              });
-              if (isExist === false) {
-                addDoc(collection(db, 'chatRooms'), {
-                  members: {member1: auth.currentUser.uid, member2: item.id},
-                  messages: [],
-                }).then(docRef => {
-                  console.log('Document written with ID: ', docRef.id);
-                  dispatch(setAllChatRooms([auth.currentUser.uid, item.id]));
-                });
-              }
+              setChatRoom(item.id);
             }}
           />
         )}

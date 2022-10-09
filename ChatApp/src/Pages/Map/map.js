5e0ce4e99e53d8490f574c674b24320b49d72701
Atakanz <atakanzer@gmail.com>
2022-10-09 08:36:56 +0300

    Bigger map view added.

--- ChatApp/src/Pages/Map/map.js
+++ ChatApp/src/Pages/Map/map.js
@@ -1,11 +1,31 @@
 import React from 'react';
-import {SafeAreaView, Text} from 'react-native';
+import MapView, {Marker} from 'react-native-maps';
+import {SafeAreaView} from 'react-native';
 import styles from './map.style';
+import {auth} from '../../../config';
 
-const Map = () => {
+const Map = ({navigation, route}) => {
+  const {longitude, latitude, sender} = route.params;
+
   return (
     <SafeAreaView style={styles.container}>
-      <Text>Hello Map!</Text>
+      <MapView
+        initialRegion={{
+          latitude: latitude,
+          longitude: longitude,
+          latitudeDelta: 0.0922,
+          longitudeDelta: 0.0421,
+        }}
+        style={styles.map}
+        maxZoomLevel={15}>
+        <Marker
+          coordinate={{
+            latitude: latitude,
+            longitude: longitude,
+          }}
+          pinColor={sender === auth.currentUser.uid ? '#00B050' : '#1A5BAA'}
+        />
+      </MapView>
     </SafeAreaView>
   );
 };

07fe47e35b4bc3157da479f77e13e43d59d60919
Atakanz <atakanzer@gmail.com>
2022-10-09 08:50:46 +0300

    Allusers slicer removed.

--- ChatApp/src/Management/Features/userSlice.js
+++ ChatApp/src/Management/Features/userSlice.js
@@ -3,7 +3,6 @@ import AsyncStorage from '@react-native-async-storage/async-storage';
 
 const initialState = {
   user: [],
-  allUsers: [],
 };
 
 export const userSlice = createSlice({
@@ -11,12 +10,6 @@ export const userSlice = createSlice({
   initialState,
   reducers: {
     setUser: (state, action) => {
-      state.user = action.payload;
-    },
-    setAllUsers: (state, action) => {
-      state.allUsers = action.payload;
-    },
-    logIn: (state, action) => {
       state.user = action.payload;
       AsyncStorage.setItem('savedUser', JSON.stringify(action.payload));
     },
@@ -27,6 +20,6 @@ export const userSlice = createSlice({
   },
 });
 
-export const {setUser, setAllUsers, logIn, logOut} = userSlice.actions;
+export const {setUser, logOut} = userSlice.actions;
 
 export default userSlice.reducer;

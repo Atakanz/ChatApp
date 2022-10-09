3acedc51e217a5fd1da80c65eb1842c37309f5a1
Atakanz <atakanzer@gmail.com>
2022-10-09 08:27:34 +0300

    Personal username selection added.

--- ChatApp/src/Pages/SignUp/signUp.js
+++ ChatApp/src/Pages/SignUp/signUp.js
@@ -4,15 +4,22 @@ import styles from './signUp.style';
 import LoginForm from '../../Components/LoginForm';
 import {useSelector} from 'react-redux';
 import {createUserWithEmailAndPassword} from 'firebase/auth';
-import {setDoc, doc} from 'firebase/firestore';
+import {
+  setDoc,
+  doc,
+  collection,
+  query,
+  where,
+  getDocs,
+} from 'firebase/firestore';
 import {auth, db} from '../../../config';
 
 const SignUp = ({navigation}) => {
   const [newUserEmail, setNewUserEmail] = useState('');
   const [newUserPassword, setNewUserPassword] = useState('');
   const [newUserRePassword, setNewUserRePassword] = useState('');
+  const [newName, setName] = useState('');
   const [newUserName, setNewUserName] = useState('');
-  const [newUserSurname, setNewUserSurname] = useState('');
   const theme = useSelector(state => state.theme.theme);
 
   const registerUser = async () => {
@@ -22,20 +29,33 @@ const SignUp = ({navigation}) => {
           email: response.user.email,
           id: response.user.uid,
           name: newUserName,
-          surname: newUserSurname,
+          username: newUserName,
           photoUrl: null,
         });
       },
     );
   };
 
-  const signUpButton = () => {
-    if (newUserRePassword === newUserPassword) {
-      registerUser();
-      navigation.navigate('SignIn');
-    } else {
-      Alert.alert('InTouch', 'Passwords do not match.');
-    }
+  const signUpButton = async () => {
+    const q = query(
+      collection(db, 'users'),
+      where('username', '==', newUserName),
+    );
+    await getDocs(q).then(res => {
+      if (res.docs.length > 0) {
+        Alert.alert(
+          'DeepTalk',
+          'User name exists, please specify another one.',
+        );
+        return;
+      } else if (newUserRePassword !== newUserPassword) {
+        Alert.alert('DeepTalk', 'Passwords do not match.');
+        return;
+      } else if (newUserRePassword === newUserPassword) {
+        registerUser();
+        navigation.navigate('SignIn');
+      }
+    });
   };
 
   return (
@@ -46,18 +66,18 @@ const SignUp = ({navigation}) => {
         holder2="Password"
         holder3="Password again"
         holder4="Name"
-        holder5="Surname"
+        holder5="User name"
         name1="Sign Up"
         value1={newUserEmail}
         value2={newUserPassword}
         value3={newUserRePassword}
-        value4={newUserName}
-        value5={newUserSurname}
+        value4={newName}
+        value5={newUserName}
         emailFormTask={setNewUserEmail}
         passwordFormTask={setNewUserPassword}
         repasswordFormTask={setNewUserRePassword}
-        userNameFormTask={setNewUserName}
-        userSurnameFormTask={setNewUserSurname}
+        userNameFormTask={setName}
+        userSurnameFormTask={setNewUserName}
         task1={signUpButton}
         securityFalse={false}
         securityTrue={true}
